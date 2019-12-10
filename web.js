d06ca146a96b39f5106f7ced8ae8a99d892152d2
pujayeole <58590445+pujayeole@users.noreply.github.com>
2019-12-10 05:27:27 -0800

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,7 @@
-var http = require('http'); // 1 - Import Node.js core module
+var http = require('http');
 
-var server = http.createServer(function (req, res) {   // 2 - creating server
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(8080);
 
-    //handle incomming requests here..
-
-});
-
-server.listen(5000); //3 - listen for any incoming requests
-
-console.log('Node.js web server at port 5000 is running..')
